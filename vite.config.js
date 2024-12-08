450fb7ba0855d3882e5859959461f98680602c42
Sumit Dalai <dalaisumit611@gmail.com>
2024-12-08 19:25:29 +0530

    vite test setup

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+    css: true,
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+    testMatch: ["**/*.{test,spec}.{js,jsx,ts,tsx}"],
+  },
+});
