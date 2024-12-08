5091325f307fd4b7a9ea54e47340206064b79b6e
Sumit Dalai <dalaisumit611@gmail.com>
2024-12-08 15:29:12 +0530

    for movies and tv shows pages for now we set commin gsoon

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./pages.scss";
+const Movies = () => {
+  return (
+    <>
+      <div className="main-movie">
+        <h1>Coming Soon.......</h1>
+      </div>
+    </>
+  );
+};
+
+export default Movies;
---
+++ src/pages/TvShows.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TvShows = () => {
+  return (
+    <>
+      <div className="main-tv">
+        <h1>Coming Soon......</h1>
+      </div>
+    </>
+  );
+};
+
+export default TvShows;
